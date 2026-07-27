ca66a4b8070c7b033e7637f81d05d24233c722b3
haveheadd <belousovajob@gmail.com>
2026-07-27 16:19:49 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
+/// <reference types="vite/client" />
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './style.css';
-createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
